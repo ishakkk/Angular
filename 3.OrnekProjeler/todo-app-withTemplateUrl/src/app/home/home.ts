acf56c38db5850a27780907962721f6588e85a84
ishakkk <ishakgzl68@gmail.com>
2026-06-24 14:57:29 +0300

    TodoWithHtmlUrl

--- 3.OrnekProjeler/todo-app-withTemplateUrl/src/app/home/home.ts
+++ 3.OrnekProjeler/todo-app-withTemplateUrl/src/app/home/home.ts
@@ -7,4 +7,33 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+insertData:string="";
+
+toDos:string[]=[];
+updateIndex:number=0;
+updateData:string="";
+isFormUpdate:boolean=false;
+  save()
+  {
+    this.toDos.push(this.insertData)
+    this.insertData="";
+    
+  }
+  get(index:number)
+  {
+    this.updateIndex=index;
+    this.updateData=this.toDos[index];
+    this.isFormUpdate=true;
+  }
+  delete(index:number)
+  {
+    this.toDos.splice(index,1)
+  }
+  update()
+  {
+    this.toDos[this.updateIndex]=this.updateData
+    this.isFormUpdate=false;
+  }
+}
